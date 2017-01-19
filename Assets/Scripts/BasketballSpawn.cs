018b217a49debcf2291998d2d6778f9290707ba3
godfatherfrancis <francis.aarons@gmail.com>
2017-01-19 00:18:36 -0500

    Prefabs, Scripts, Material added

---
+++ Assets/Scripts/BasketballSpawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasketballSpawn : MonoBehaviour {
+
+    public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
