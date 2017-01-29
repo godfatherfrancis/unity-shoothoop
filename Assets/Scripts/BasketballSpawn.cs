17a24482b49f815c4ef2182debb8fefb0c9a9105
godfatherfrancis <francis.aarons@gmail.com>
2017-01-29 18:30:33 -0500

    take mouse input

--- Assets/Scripts/BasketballSpawn.cs
+++ Assets/Scripts/BasketballSpawn.cs
@@ -7,14 +7,27 @@ public class BasketballSpawn : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Instantiate(ballPrefab);
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Instantiate(ballPrefab);
+        float speed = 5.0f;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody rigidbody = instance.GetComponent<Rigidbody>();
+            rigidbody.velocity = Vector3.left * speed;
+        }
+    }
+
+    private void SpawnBall(KeyCode keyCode)
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody rigidbody = instance.GetComponent<Rigidbody>();
+            // rigidbody.velocity = Vector3.left * vel;
         }
     }
 }
---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotationSpeed = 5.0f;
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        transform.rotation = Quaternion.Euler(-mouseY, mouseX, 0) * transform.rotation;
+    }
+}
